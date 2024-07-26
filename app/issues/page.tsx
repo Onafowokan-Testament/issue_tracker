c2deea7efa7c00e9cc51074cac81c0206120e5d3
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-07-26 18:53:48 +0100

    set Up radixUI

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,7 +1,8 @@
 import React from "react";
+import { Button } from "@radix-ui/themes";
 
 const IssueTracker = () => {
-  return <div>IssueTracker</div>;
+  return <Button>Les go</Button>;
 };
 
 export default IssueTracker;
--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import "@radix-ui/themes/styles.css";
+import { Theme } from "@radix-ui/themes";
 import Navbar from "./Navbar";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -18,9 +20,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Navbar />
+        <Theme>
+          <Navbar />
 
-        <main>{children}</main>
+          <main>{children}</main>
+        </Theme>
       </body>
     </html>
   );
