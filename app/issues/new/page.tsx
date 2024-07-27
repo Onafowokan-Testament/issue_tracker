204964c1228d0ee51f0538fbe8bb249f4294e056
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-07-27 16:36:52 +0100

    client side error handling

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,33 +1,41 @@
 "use client";
 
-import { Button, Callout, TextArea, TextField } from "@radix-ui/themes";
+import { Button, Callout, TextArea, TextField, Text } from "@radix-ui/themes";
 import SimpleMDE from "react-simplemde-editor";
 import "easymde/dist/easymde.min.css";
 import { Controller, useForm } from "react-hook-form";
 import axios from "axios";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
-interface IssueForm {
-  title: string;
-  description: string;
-}
+import { zodResolver } from "@hookform/resolvers/zod";
+import createIssueSchema from "@/app/validationSchema";
+import { z } from "zod";
+
+type IssueForm = z.infer<typeof createIssueSchema>;
 
 const createNewIssue = () => {
   const router = useRouter();
 
   const [error, setError] = useState("");
 
-  const { register, control, handleSubmit } = useForm<IssueForm>();
+  const {
+    register,
+    control,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<IssueForm>({
+    resolver: zodResolver(createIssueSchema),
+  });
 
   return (
-    <div className="max-w-xl mt-5">
+    <div className="max-w-xl my-5">
       {error && (
         <Callout.Root color="red">
           <Callout.Text>{error}</Callout.Text>
         </Callout.Root>
       )}
       <form
-        className="space-y-5 max-w-xl"
+        className="space-y-3 max-w-xl"
         onSubmit={handleSubmit(async (data) => {
           try {
             await axios.post("/api/issues", data);
@@ -43,16 +51,31 @@ const createNewIssue = () => {
           className="my-5"
           {...register("title")}
         ></TextField.Root>
+        {errors.title && (
+          <Text color="red" as="p">
+            {errors.title.message}
+          </Text>
+        )}
 
         <Controller
           name="description"
           control={control}
           render={({ field }) => (
-            <SimpleMDE placeholder="Description" {...field} />
+            <SimpleMDE placeholder="Description" {...field} className=" my-0" />
           )}
         />
+        {errors.description && (
+          <Text color="red" as="p">
+            {errors.description.message}
+          </Text>
+        )}
 
-        <Button>Create new Issue</Button>
+        <Button
+          className="space-y-0
+        "
+        >
+          Create new Issue
+        </Button>
       </form>
     </div>
   );
