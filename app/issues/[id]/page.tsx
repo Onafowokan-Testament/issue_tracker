eebf59af88ccd30c27ef252e29c6d2efa54a9c27
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-07-29 11:43:57 +0100

    loading akeleton for new issue and viewin issue

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,6 +4,7 @@ import { Card, Flex, Heading, Text } from "@radix-ui/themes";
 import { notFound } from "next/navigation";
 import React from "react";
 import ReactMarkdown from "react-markdown";
+import delay from "delay";
 
 interface Props {
   params: {
