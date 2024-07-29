3d34c3bc0cf8f58795604096325a6e51f3972644
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-07-29 12:34:13 +0100

    Refactor;Apply the SRP

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,12 @@
+import { Button, Link } from "@radix-ui/themes";
+import React from "react";
+
+const EditIssueButton = ({ issueId }: { issueId: number }) => {
+  return (
+    <Link href={`'issues/${issueId}/edit'`}>
+      <Button>Edit</Button>
+    </Link>
+  );
+};
+
+export default EditIssueButton;
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,10 +1,11 @@
 import StatusIssueBadge from "@/app/components/StatusIssueBadge";
 import prisma from "@/prisma/client";
-import { Card, Flex, Heading, Text } from "@radix-ui/themes";
+import { Box, Button, Card, Flex, Grid, Heading, Text } from "@radix-ui/themes";
+import Link from "next/link";
 import { notFound } from "next/navigation";
-import React from "react";
 import ReactMarkdown from "react-markdown";
-import delay from "delay";
+import EditIssueButton from "./EditIssueButton";
+import IssueDetails from "./IssueDetails";
 
 interface Props {
   params: {
@@ -20,17 +21,14 @@ const page = async ({ params }: Props) => {
   if (!issue) notFound();
 
   return (
-    <div>
-      <Heading> {issue.title}</Heading>
-      <Flex className="space-x-3 " my="2">
-        <StatusIssueBadge status={issue.status}></StatusIssueBadge>
-        <Text>{issue.createdAt.toDateString()}</Text>
-      </Flex>
-
-      <Card className="prose" mt={"4"}>
-        <ReactMarkdown>{issue.description}</ReactMarkdown>
-      </Card>
-    </div>
+    <Grid columns={{ initial: "1", md: "2" }} gap={"5"}>
+      <Box>
+        <IssueDetails issue={issue}></IssueDetails>
+      </Box>
+      <Box>
+        <EditIssueButton issueId={issue.Id} />
+      </Box>
+    </Grid>
   );
 };
 
