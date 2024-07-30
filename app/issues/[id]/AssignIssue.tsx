c389c8a9baa2f4696cf5582abff647b3b92c9979
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-07-30 20:55:32 +0100

    api for getting users

--- app/issues/[id]/AssignIssue.tsx
+++ app/issues/[id]/AssignIssue.tsx
@@ -1,7 +1,19 @@
-import { Select } from "@radix-ui/themes";
-import React from "react";
+"use client";
 
-const AssignIssue = ({ id }: { id: number }) => {
+import { User } from "@prisma/client";
+import { Select } from "@radix-ui/themes";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const AssignIssue = async ({ id }: { id: number }) => {
+  const [users, setUsers] = useState<User[]>([]);
+  useEffect(() => {
+    const fetch_users = async () => {
+      const data = (await axios.get<User[]>("/api/users")).data;
+      setUsers(data);
+    };
+    fetch_users();
+  }, []);
   return (
     <Select.Root>
       <Select.Trigger placeholder="Assign..." />
@@ -9,9 +21,11 @@ const AssignIssue = ({ id }: { id: number }) => {
       <Select.Content>
         <Select.Group>
           <Select.Label>users</Select.Label>
-          <Select.Item value="user 1">User 1</Select.Item>
-          <Select.Item value="user 2">User 2</Select.Item>
-          <Select.Item value="user 3">User 3</Select.Item>
+          {users.map((user) => (
+            <Select.Item value={user.id} key={user.id}>
+              {user.name}
+            </Select.Item>
+          ))}
         </Select.Group>
       </Select.Content>
     </Select.Root>
