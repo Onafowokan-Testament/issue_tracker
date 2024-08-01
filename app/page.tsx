38c91bce0435fc00a2b409d0f31c27897b3cdd26
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-08-01 20:43:28 +0100

    Build issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,8 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 import LatesetIssues from "./LatesetIssues";
+import IssueSummary from "./IssueSummary";
+import prisma from "@/prisma/client";
 
 interface Props {
   searchParams: {
@@ -8,11 +10,20 @@ interface Props {
   };
 }
 
-export default function Home({ searchParams }: Props) {
+export default async function Home({ searchParams }: Props) {
+  const open = await prisma.issue.count({
+    where: { status: "OPEN" },
+  });
+  const closed = await prisma.issue.count({
+    where: { status: "CLOSED" },
+  });
+  const in_progress = await prisma.issue.count({
+    where: { status: "IN_PROGRESS" },
+  });
   return (
     <>
       <div>Hello world</div>
-      <LatesetIssues />
+      <IssueSummary open={open} inProgress={in_progress} closed={closed} />
     </>
   );
 }
