1438e790e23d3ee829eb14cd6a1a964856fd8b82
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-08-02 00:10:33 +0100

    worked on database

--- prisma/migrations/20240726170917_corrected_model/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` MODIFY `description` TEXT NOT NULL;
--- prisma/migrations/20240730215539_mapping_issues_to_user/migration.sql
+++
@@ -1,6 +0,0 @@
--- AlterTable
-ALTER TABLE `issue` ADD COLUMN `assignedUserId` VARCHAR(255) NULL,
-    MODIFY `title` VARCHAR(250) NOT NULL;
-
--- AddForeignKey
-ALTER TABLE `Issue` ADD CONSTRAINT `Issue_assignedUserId_fkey` FOREIGN KEY (`assignedUserId`) REFERENCES `User`(`id`) ON DELETE SET NULL ON UPDATE CASCADE;
