cdb0b71e5c0ea941ba62299c447ee7dfa76ebf3d
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-08-01 13:42:07 +0100

    make columns sortable

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -1,26 +1,51 @@
 import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import prisma from "@/prisma/client";
-import { Status } from "@prisma/client";
+import { Issue, Status } from "@prisma/client";
 import delay from "delay";
 import StatusIssueBadge from "../components/StatusIssueBadge";
 import IssueAction from "./IssueAction";
 import Link from "../components/Link";
+import classNames from "classnames";
+import NextLink from "next/link";
+import { FaArrowUp } from "react-icons/fa6";
 
 const IssueTracker = async ({
   searchParams,
 }: {
-  searchParams: { status: Status };
+  searchParams: { status: Status; orderBy: keyof Issue };
 }) => {
   const statuses = Object.values(Status);
 
   const status = statuses.includes(searchParams.status)
     ? searchParams.status
     : undefined;
+
+  const orderByField = searchParams.orderBy || "createdAt";
   const issues = await prisma.issue.findMany({
     where: { status: status },
+    orderBy: {
+      [orderByField]: "desc",
+    },
   });
 
+  // const select = status ? status : " ";
+  // const query = select === "all" ? "" : `?status=${status}`;
+
+  // /issues?status=?
+  const headers: {
+    label: string;
+    value: keyof Issue;
+    classNames?: string;
+  }[] = [
+    { label: "Issue", value: "title", classNames: "" },
+    { label: "Status", value: "status", classNames: " hidden md:table-cell" },
+    {
+      label: "Created",
+      value: "createdAt",
+      classNames: " hidden md:table-cell",
+    },
+  ];
   await delay(2000);
   return (
     <div>
@@ -28,13 +53,21 @@ const IssueTracker = async ({
       <Table.Root variant="surface">
         <Table.Header>
           <Table.Row>
-            <Table.ColumnHeaderCell>Issue</Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className=" hidden md:table-cell">
-              Status
-            </Table.ColumnHeaderCell>
-            <Table.ColumnHeaderCell className=" hidden md:table-cell">
-              Created
-            </Table.ColumnHeaderCell>
+            {headers.map((header) => (
+              <Table.ColumnHeaderCell
+                key={header.label}
+                className={header.classNames}
+              >
+                <NextLink
+                  href={{ query: { ...searchParams, orderBy: header.value } }}
+                >
+                  {header.label}
+                </NextLink>
+                {header.value === searchParams.orderBy && (
+                  <FaArrowUp className="inline"></FaArrowUp>
+                )}
+              </Table.ColumnHeaderCell>
+            ))}
           </Table.Row>
         </Table.Header>
 
