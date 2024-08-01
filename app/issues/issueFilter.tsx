f1f3bfcc27a147a39286563f67533cdadd54ede6
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-08-01 14:42:13 +0100

    refactor: sorting issues

--- app/issues/issueFilter.tsx
+++ app/issues/issueFilter.tsx
@@ -2,7 +2,7 @@
 
 import { Status } from "@prisma/client";
 import { Select } from "@radix-ui/themes";
-import { useRouter } from "next/navigation";
+import { useRouter, useSearchParams } from "next/navigation";
 import React from "react";
 
 const statuses: { label: string; value?: Status | null }[] = [
@@ -14,13 +14,21 @@ const statuses: { label: string; value?: Status | null }[] = [
 
 const IssueFilter = () => {
   const router = useRouter();
+  const searchParams = useSearchParams();
   return (
     <div>
       <Select.Root
+        defaultValue={searchParams.get("status") || " "}
         onValueChange={(status) => {
+          const params = new URLSearchParams();
+          if (status) params.append("status", status);
+          if (searchParams.get("orderBy"))
+            params.append("orderBy", searchParams.get("orderBy")!);
+
           const select = status ? status : " ";
           const query = select === "all" ? "" : `?status=${status}`;
-          router.push("/issues" + query);
+          const url = params.size ? "?" + params.toString() : "";
+          router.push("/issues" + url);
         }}
       >
         <Select.Trigger placeholder="Filter by status..."></Select.Trigger>
