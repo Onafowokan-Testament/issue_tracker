99c6d6e47c76f44127c46eb02aef58d74363c547
Onafowokan-Testament <onafowokan.testament@stu.cu.edu.ng>
2024-08-01 18:15:20 +0100

    implement Pagination

--- app/components/Pagination.tsx
+++ app/components/Pagination.tsx
@@ -1,4 +1,7 @@
+"use client";
 import { Button, Flex, Text } from "@radix-ui/themes";
+import { useParams, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import React from "react";
 import { FaArrowLeft, FaArrowRight } from "react-icons/fa6";
 import { IoChevronBackOutline, IoChevronForwardSharp } from "react-icons/io5";
@@ -14,26 +17,57 @@ interface Prop {
 }
 
 const Pagination = ({ pageSize, currentPage, itemCount }: Prop) => {
+  const router = useRouter();
+  const search_params = useSearchParams();
+
   const total_page = Math.ceil(itemCount / pageSize);
   if (total_page <= 1) return null;
+
+  const changePage = (page: number) => {
+    const params = new URLSearchParams(search_params);
+
+    params.set("page", page.toString());
+    router.push("?" + params.toString());
+  };
+
   return (
     <Flex align={"center"} className="space-x-3">
       <Text size={"2"}>
         Page {currentPage} of {total_page}{" "}
       </Text>
 
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(1)}
+      >
         <MdOutlineKeyboardDoubleArrowLeft />
       </Button>
-      <Button color="gray" variant="soft" disabled={currentPage === 1}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === 1}
+        onClick={() => changePage(currentPage - 1)}
+      >
         <IoChevronBackOutline />
       </Button>
 
-      <Button color="gray" variant="soft" disabled={currentPage === total_page}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === total_page}
+        onClick={() => changePage(currentPage + 1)}
+      >
         <IoChevronForwardSharp />
       </Button>
 
-      <Button color="gray" variant="soft" disabled={currentPage === total_page}>
+      <Button
+        color="gray"
+        variant="soft"
+        disabled={currentPage === total_page}
+        onClick={() => changePage(total_page)}
+      >
         <MdOutlineKeyboardDoubleArrowRight />
       </Button>
     </Flex>
--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,22 @@
 import Image from "next/image";
 import Pagination from "./components/Pagination";
 
-export default function Home() {
+interface Props {
+  searchParams: {
+    page: string;
+  };
+}
+
+export default function Home({ searchParams }: Props) {
   return (
     <>
       <div>Hello world</div>
 
-      <Pagination pageSize={10} currentPage={1} itemCount={40}></Pagination>
+      <Pagination
+        pageSize={10}
+        currentPage={parseInt(searchParams.page)}
+        itemCount={40}
+      ></Pagination>
     </>
   );
 }
